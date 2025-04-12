57ff5114e7616c6ee0b81e07388b7882579648f0
ardiansyah1506 <fauziardiansyah302@gmail.com>
2025-04-12 12:38:31 +0700

    fix

--- app/Http/Controllers/IndeksPenyakitController.php
+++ app/Http/Controllers/IndeksPenyakitController.php
@@ -8,29 +8,30 @@ use Illuminate\Http\Request;
 class IndeksPenyakitController extends Controller
 {
     public function index(Request $request)
-    {
-        $data = MasterIndeksing::join('icd10_primary','icd10_primary.id','master_indeksing.icd10primary')->query();
+{
+    $data = MasterIndeksing::join('icd10_primary','icd10_primary.id','master_indeksing.icd10primary');
 
-        if ($request->filled('jenis_kunjungan')) {
-            $data->where('jenis_kunjungan', '=', $request->jenis_kunjungan );
-        }
-
-        if ($request->filled('nama_penyakit')) {
-            $data->orWhere('icd10_primary.nama', 'like', '%' . $request->nama_penyakit . '%');
-            $data->orWhere('icd10_secondary.kode', 'like', '%' . $request->nama_penyakit . '%');
-        }
-    
-        // Filter berdasarkan range tanggal kunjungan
-        if ($request->filled('tgl_awal') && $request->filled('tgl_akhir')) {
-            $data->whereBetween('tgl_kunjungan', [$request->tgl_awal, $request->tgl_akhir]);
-        }
-        $data = $data->get();
-        $dataCount = $data->count();
-    if(empty($request->all())){
-        return view('rawat-jalan.indeksing-penyakit.index', compact('data','dataCount'));
+    if ($request->filled('jenis_kunjungan')) {
+        $data->where('jenis_kunjungan', $request->jenis_kunjungan );
     }
+
+    if ($request->filled('nama_penyakit')) {
+        $data->where(function($q) use ($request) {
+            $q->where('icd10_primary.nama', 'like', '%' . $request->nama_penyakit . '%')
+              ->orWhere('icd10_primary.kode', 'like', '%' . $request->nama_penyakit . '%');
+        });
+    }
+
+    if ($request->filled('tgl_awal') && $request->filled('tgl_akhir')) {
+        $data->whereBetween('tgl_kunjungan', [$request->tgl_awal, $request->tgl_akhir]);
+    }
+
+    $data = $data->get();
+    $dataCount = $data->count();
+
     return view('rawat-jalan.indeksing-penyakit.index', compact('data','dataCount'));
-     }
+}
+
 
    
 }
