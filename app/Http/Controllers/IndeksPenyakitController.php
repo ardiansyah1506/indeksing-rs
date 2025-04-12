c18baa4a463e305dbb06d4996e8197b5b2ff7d0e
ardiansyah1506 <fauziardiansyah302@gmail.com>
2025-04-12 12:36:48 +0700

    fix

--- app/Http/Controllers/IndeksPenyakitController.php
+++ app/Http/Controllers/IndeksPenyakitController.php
@@ -9,34 +9,28 @@ class IndeksPenyakitController extends Controller
 {
     public function index(Request $request)
     {
-        $data = MasterIndeksing::query();
-    
-        // Filter jenis kunjungan
+        $data = MasterIndeksing::join('icd10_primary','icd10_primary.id','master_indeksing.icd10primary')->query();
+
         if ($request->filled('jenis_kunjungan')) {
-            $data->where('jenis_kunjungan', '=', $request->jenis_kunjungan);
+            $data->where('jenis_kunjungan', '=', $request->jenis_kunjungan );
         }
-    
-        // Filter nama penyakit dari dua tabel
+
         if ($request->filled('nama_penyakit')) {
-            $data->join('icd10_primary', 'master_indeksing.icd10primary', '=', 'icd10_primary.id')
-                 ->join('icd10_secondary', 'master_indeksing.icd10secondary', '=', 'icd10_secondary.id')
-                 ->where(function ($query) use ($request) {
-                     $query->where('icd10_primary.nama', 'like', '%' . $request->nama_penyakit . '%')
-                           ->orWhere('icd10_secondary.nama', 'like', '%' . $request->nama_penyakit . '%');
-                 });
+            $data->orWhere('icd10_primary.nama', 'like', '%' . $request->nama_penyakit . '%');
+            $data->orWhere('icd10_secondary.kode', 'like', '%' . $request->nama_penyakit . '%');
         }
     
         // Filter berdasarkan range tanggal kunjungan
         if ($request->filled('tgl_awal') && $request->filled('tgl_akhir')) {
             $data->whereBetween('tgl_kunjungan', [$request->tgl_awal, $request->tgl_akhir]);
         }
-    
         $data = $data->get();
         $dataCount = $data->count();
-    
-        return view('rawat-jalan.indeksing-penyakit.index', compact('data', 'dataCount'));
+    if(empty($request->all())){
+        return view('rawat-jalan.indeksing-penyakit.index', compact('data','dataCount'));
     }
-    
+    return view('rawat-jalan.indeksing-penyakit.index', compact('data','dataCount'));
+     }
 
    
 }
