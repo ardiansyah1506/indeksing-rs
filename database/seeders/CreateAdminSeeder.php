0b719c424c4350170b60f0fb26785216333712ef
ardiansyah1506 <fauziardiansyah302@gmail.com>
2025-04-15 22:48:41 +0700

    feat: update seeder

--- database/seeders/CreateAdminSeeder.php
+++ database/seeders/CreateAdminSeeder.php
@@ -16,7 +16,7 @@ class CreateAdminSeeder extends Seeder
     public function run(): void
     {
             $user = User::create([
-                'name' => 'Admin', 
+                'name' => 'admin', 
                 'email' => 'admin@gmail.com',
                 'password' => bcrypt('123456')
             ]);
